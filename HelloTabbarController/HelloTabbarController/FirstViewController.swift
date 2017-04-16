47f04731a9491671cf07c947593cb02bc3ad72d1
Viet Nguyen Tran <vietnt88@gmail.com>
2017-04-16 23:57:46 +0700

    add code

---
+++ HelloTabbarController/HelloTabbarController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HelloTabbarController
+//
+//  Created by Viet Nguyen Tran on 4/15/17.
+//  Copyright © 2017 iossimple. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HelloTabbarController/HelloTabbarController/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HelloTabbarController
+//
+//  Created by Viet Nguyen Tran on 4/15/17.
+//  Copyright © 2017 iossimple. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
